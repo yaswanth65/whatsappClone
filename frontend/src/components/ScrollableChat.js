6061036867b653929c7311478028ade7f416e0fb
Yaswanth Kancharla <yaswanth.kancharla65@gmail.com>
2025-06-16 17:52:24 +0530

    added time n date

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -8,17 +8,29 @@ import {
   isSameUser,
 } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
+import { Text } from "@chakra-ui/layout";
 
 const ScrollableChat = ({ messages }) => {
   const { user } = ChatState();
+
+  const formatTime = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  };
 
   return (
     <ScrollableFeed>
       {messages &&
         messages.map((m, i) => (
-          <div style={{ display: "flex" }} key={m._id}>
-            {(isSameSender(messages, m, i, user._id) ||
-              isLastMessage(messages, i, user._id)) && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: m.sender._id === user._id ? "flex-end" : "flex-start",
+              marginBottom: "10px"
+            }}
+            key={m._id}
+          >
+            {m.sender._id !== user._id && (
               <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
                 <Avatar
                   mt="7px"
@@ -30,20 +42,51 @@ const ScrollableChat = ({ messages }) => {
                 />
               </Tooltip>
             )}
-            <span
-              style={{
-                backgroundColor: `${
-                  m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
-                }`,
-                marginLeft: isSameSenderMargin(messages, m, i, user._id),
-                marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
-                borderRadius: "20px",
-                padding: "5px 15px",
-                maxWidth: "75%",
-              }}
-            >
-              {m.content}
-            </span>
+            <div style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: "75%"
+            }}>
+              <span
+                style={{
+                  backgroundColor: `${m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                    }`,
+                  borderRadius: "20px",
+                  padding: "5px 15px",
+                  marginLeft: m.sender._id === user._id ? "auto" : "0",
+                  marginRight: m.sender._id === user._id ? "0" : "auto",
+                }}
+              >
+                {m.content}
+              </span>
+              <div style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: m.sender._id === user._id ? "flex-end" : "flex-start",
+                marginTop: "2px",
+                fontSize: "0.75rem",
+                color: "#666"
+              }}>
+                <Text fontSize="xs" mr={2}>
+                  {m.sender._id === user._id ? "You" : m.sender.name}
+                </Text>
+                <Text fontSize="xs">
+                  {formatTime(m.createdAt)}
+                </Text>
+              </div>
+            </div>
+            {m.sender._id === user._id && (
+              <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                <Avatar
+                  mt="7px"
+                  ml={1}
+                  size="sm"
+                  cursor="pointer"
+                  name={m.sender.name}
+                  src={m.sender.pic}
+                />
+              </Tooltip>
+            )}
           </div>
         ))}
     </ScrollableFeed>
